b9998348e6c26b61c09dc2ffe2cd4f13b5f046f9
ChaoTang0330 <happy.tang.chao@outlook.com>
2024-02-28 14:03:56 +0000

    Update example

--- examples/ex_usara.m
+++ examples/ex_usara.m
@@ -8,9 +8,9 @@ cd(path)
 cd ..
 
 config = ['.', filesep, 'config', filesep, 'usara_sim.json'];
-msFile = ['.', filesep, 'examples', filesep, 'simulated_measurements', filesep, 'dt8', filesep, '3c353_lrs_1.0_seed_0.mat'];
+dataFile = ['.', filesep, 'examples', filesep, 'simulated_measurements', filesep, 'dt8', filesep, '3c353_lrs_1.0_seed_0.mat'];
 resultPath = ['.', filesep, 'results', filesep, '3c353_dt8_seed0', filesep, 'uSARA']; 
 algorithm = 'usara';
 RunID = 0;
 
-run_imager(config, 'msFile', msFile, 'resultPath', resultPath, 'runID', 0)
+run_imager(config, 'dataFile', dataFile, 'resultPath', resultPath, 'runID', 0)
